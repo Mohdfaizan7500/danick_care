710564e7ccd9c0bd849c554711e52e129eddce54
mohd faizan khan <mohdfaizankhan@mohds-MacBook-Air.local>
2026-06-19 16:39:47 +0530

    corect bable file

--- babel.config.js
+++ babel.config.js
@@ -4,6 +4,7 @@ module.exports = {
     "nativewind/babel"
   ],
   plugins: [
-    'react-native-reanimated/plugin', // MUST be last
+    // 'react-native-reanimated/plugin', // MUST be last
+    'react-native-worklets/plugin', // ← not reanimated/plugin
   ],
 };
